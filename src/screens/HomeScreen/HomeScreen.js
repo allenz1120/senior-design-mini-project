0618b9359b25d7fb1477063a7653c90ae6aa89c8
liben002 <pharmashawarma@gmail.com>
2021-09-09 18:29:59 -0400

    Added serving size selection

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import axios from 'axios';
 
@@ -8,6 +8,9 @@ export default function HomeScreen(props) {
   const [scanned, setScanned] = useState(false);
   const [barcodeType, setBarcodeType] = useState(null);
   const [barcodeValue, setBarcodeValue] = useState(null);
+
+  const [promptForServings, setPromptForServings] = useState(false);
+  const [servings, setServings] = useState(1);
 
   useEffect(() => {
     (async () => {
@@ -30,6 +33,7 @@ export default function HomeScreen(props) {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
+    setPromptForServings(true);
     setBarcodeType(type);
     setBarcodeValue(data);
     console.log(data);
@@ -37,6 +41,34 @@ export default function HomeScreen(props) {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
     retrieveResult(data);
   };
+
+  const GetRecipe = () => {
+    // firebase scanned items
+  }
+
+  const PostScannedItem = ({ name, calories }) => {
+    // firebase scanned item
+    // barcode, item name, calories
+  }
+
+  const createAlert = () =>
+    Alert.alert(
+      "Choose Servings",
+      "I know this is jank AF",
+      [
+        {
+          text: "1",
+          onPress: () => setServings(1),
+        },
+        {
+          text: "2",
+          onPress: () => setServings(2),
+        },
+        { text: "3", 
+          onPress: () => setServings(3),
+        }
+      ]
+    );
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission.</Text>;
@@ -52,7 +84,8 @@ export default function HomeScreen(props) {
         style={StyleSheet.absoluteFillObject}
       />
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
+      {promptForServings && <Button title={'Choose Servings'} onPress={createAlert} />}
+    </View>    
   );
 }
 
